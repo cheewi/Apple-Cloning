82260d3f38b7a073edb5d1b62493d06bb557736d
Sochetta Van <sochettavan360@gmail.com>
2024-04-29 15:05:57 +0700

    ft : added responsiveness to the hero , and the other two sections , tried adjuusting responsiveness of nav-bar , unsuccessful , and needs improvement

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 const selectElement = (element) => document.querySelector(element);
 
-selectElement('.mobile-menu').addEventListener('click', () => {
+selectElement('.hamburger').addEventListener('click', () => {
     selectElement('header').classList.toggle('active')
 } )
