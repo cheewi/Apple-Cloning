5a332255b30cf9c44f72bb6d5535d538c59be11b
Sochetta Van <sochettavan360@gmail.com>
2024-05-23 13:10:33 +0700

    add : turned grid into component

--- components/hero-card.js
+++ components/hero-card.js
@@ -12,20 +12,20 @@ class HeroCard extends HTMLElement {
         const cardSize = this.getAttribute('card-size')
         this.innerHTML  = `
         <section class="${cardSize}">
-        <div class="apple-2030">
-            <div class="absolute w-full h-full">
-                <img src="${img}" class="w-full h-full object-cover">
-            </div>
-            <div class="${detailPosition}">
-                <h2 class="pt-9 font-semibold tracking-tight leading-[1.07143] text-[32px] tab:text-[56px]">${heading}</h2>
-                <div class="max-w-[33px] tab:max-w-[42px] text-center">
-                    <h3 class="mt-2 font-light text-[19px] des:text-[28px]">${subHeading}</h3>
+            <div class="apple-2030">
+                <div class="absolute w-full h-full">
+                    <img src="${img}" class="w-full h-full object-cover">
                 </div>
-                <div>
-                    <button class="mt-[19px] py-[10px] px-[20px] font-light bg-[#0071e3] text-white border-none rounded-full hover:bg-[#2b86e2] transition duration-200 ease-in-out text-[14px] tab:text-[17px]">Learn more</button>
+                <div class="absolute w-full h-full mx-2 flex flex-col justify-start items-center ${detailPosition}">
+                    <h2 class="pt-9 font-semibold tracking-tight leading-[1.07143] text-[32px] tab:text-[56px]">${heading}</h2>
+                    <div class="max-w-[420px] md:max-w-[720px] text-center">
+                        <h3 class="mt-2 font-light text-[19px] des:text-[28px]">${subHeading}</h3>
+                    </div>
+                    <div>
+                        <button class="mt-[19px] py-[10px] px-[20px] font-light bg-[#0071e3] text-white border-none rounded-full hover:bg-[#2b86e2] transition duration-200 ease-in-out text-[14px] tab:text-[17px]">Learn more</button>
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
             `
         } 
